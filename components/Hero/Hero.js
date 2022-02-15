0c92232bcb9e8109e3bd0fbd411800b3c5d6a369
Bwise1 <oguntoyebenjamin2@gmail.com>
2022-02-15 23:09:33 +0200

    added font-swap

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,8 +4,8 @@ export default function Hero(){
     return(
         <div className=" grid content-center text-center place-content-center h-99 lg:h-screen bg-favsent/[0.7]">
                 <div className='text-4xl tracking-tight font-umbaBold text-heroText sm:text-5xl md:text-6xl'>
-                    <h1 className=''>We Brand,</h1>
-                    <h2>With you in mind.</h2>
+                    <h1 className=''>we brand,</h1>
+                    <h2>with you in mind.</h2>
                 </div>
                 <div>
                     <p className='max-w-md p-4 font-umbaMedium text-center'>We team up with purpose driven businesses to build brands that thrive globally using compelling graphics and strategic contents.</p>
