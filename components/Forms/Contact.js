d21126fe944689071e600dc170bdec45f7f3d05c
Bwise1 <oguntoyebenjamin2@gmail.com>
2022-02-16 04:14:59 +0200

    placeholder font

--- components/Forms/Contact.js
+++ components/Forms/Contact.js
@@ -6,19 +6,19 @@ export default function Contact() {
             <form>
                 <div className='grid gap-y-6 gap-x-16 py-8 font-umbaReg'>
                     <div className="">
-                        <input className="contact-input" name="email" type="name" id="email" placeholder="Name" required />
+                        <input className="contact-input placeholder:font-umbaReg" name="email" type="name" id="email" placeholder="Name" required />
                     </div>
                     <div>
-                        <input className="contact-input" type="text" id="org" placeholder="Organization" required />
+                        <input className="contact-input placeholder:font-umbaReg" type="text" id="org" placeholder="Organization" required />
                     </div>
                     <div>
-                        <input className="contact-input" type="tel" id="tel" placeholder="Phone Number" required />
+                        <input className="contact-input placeholder:font-umbaReg" type="tel" id="tel" placeholder="Phone Number" required />
                     </div>
                     <div>
-                        <input className="contact-input" type="email" id="email" placeholder="Work Email" required />
+                        <input className="contact-input placeholder:font-umbaReg" type="email" id="email" placeholder="Work Email" required />
                     </div>
                     <div className='lg:col-span-2'>
-                        <input className="contact-input" type="text" id="message" placeholder="Message" required />
+                        <input className="contact-input placeholder:font-umbaReg" type="text" id="message" placeholder="Message" required />
                     </div>
                     <div>
                         <button className='button py-2 px-4'>Submit</button>
