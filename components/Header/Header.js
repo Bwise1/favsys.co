a4be7b51a96769688f31a0ae7c4e8c5d14f782b7
Bwise1 <oguntoyebenjamin2@gmail.com>
2022-02-21 17:04:25 +0200

    Menu

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,52 +1,208 @@
 import React, { useState } from "react";
-import Link from "next/link";
+import { Link } from "react-scroll";
 import Image from "next/image";
+import { Popover, Transition } from '@headlessui/react';
 import Logo from "../../public/logo.svg";
 
 
 export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="bg-favsent/[0.7] ">
-            <header className="flex justify-between container font-umbaMedium items-start ">
-                <div className="flex items-center space-x-5 mt-4">
-                    <Link href="/">
-                        <a>
-                            <Image className="cursor-pointer" src={Logo} alt="Logo" height={39} width={123} />
-                        </a>
-                    </Link>
-                </div>
-                <div className="hidden md:flex space-x-5 mt-4">
-                    <Link href="/#projects"><a><h3 className="cursor-pointer hover:text-heroText">About</h3></a></Link>
-                    <Link href="/#projects"><a><h3 className="cursor-pointer hover:text-heroText">Projects</h3></a></Link>
-                    <Link href="/#contact"><a><h3 className="cursor-pointer hover:text-heroText">Contact</h3></a></Link>
-                </div>
-                <div className="  md:hidden space-x-5 mt-4">
-                    {/*  */}
-                    <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        type="button"
-                        className=" inline-flex items-center justify-center p-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-800 focus:ring-white"
-                        aria-controls="mobile-menu"
-                        aria-expanded="false"
-                    >
+        <div>
+            <nav className=" shadow-sm fixed w-full  z-10">
+                <div className="w-full">
+                    <div className="flex items-center container m-auto font-umbaMedium h-20 w-full">
+                        <div className="flex items-center justify-between w-full">
+                            <div className="flex items-center space-x-5 mt-4">
+                                <Link href="/">
+                                    <a>
+                                        <Image className="cursor-pointer" src={Logo} alt="Logo" height={39} width={123} />
+                                    </a>
+                                </Link>
+                            </div>
+                            <div className="hidden md:block">
+                                <div className="ml-10 flex items-baseline space-x-4">
+                                    <Link
+                                        className="cursor-pointer hover:bg-buttonPaint text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                        activeClass="Home"
+                                        href="/"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
 
-                        <svg width="30" height="29" viewBox="0 0 41 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 0.875H34.8571C37.6186 0.875 39.8571 3.11358 39.8571 5.875V7.14286H12V0.875Z" fill="black" />
-                            <path d="M0 12.0178H40.3929V18.9821H0V12.0178Z" fill="black" />
-                            <path d="M0 23.8572H40.3929V30.125H5C2.23858 30.125 0 27.8865 0 25.125V23.8572Z" fill="black" />
-                        </svg>
-                    </button>
+                                    >
+                                        Home
+                                    </Link>
+                                    <Link
+                                        activeClass="about"
+                                        to="about"
+                                        href="/#projects"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-buttonPaint text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        About
+                                    </Link>
+                                    <Link
+                                        activeClass="work"
+                                        to="work"
+                                        href="/#projects"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-buttonPaint text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Projects
+                                    </Link>
+
+                                    <Link
+                                        activeClass="Services"
+                                        to="work"
+                                        href="/"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-buttonPaint text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Services
+                                    </Link>
+
+                                    <Link
+                                        activeClass="contact"
+                                        to="contact"
+                                        href="/#contact"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-buttonPaint text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Contact
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" flex md:hidden ">
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                type="button"
+                                className="bg-blue-600 inline-flex items-center justify-center p-2 rounded-md text-white  hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-800 focus:ring-white"
+                                aria-controls="mobile-menu"
+                                aria-expanded="false"
+                            >
+                                <span className="sr-only">Open main menu</span>
+                                {!isOpen ? (
+                                    <svg
+                                        className="block h-6 w-6"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        className="block h-6 w-6"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
-                {isOpen ?(
-                <div className="bg-white px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <Link href="/#projects"><a><h3 className="cursor-pointer hover:bg-blue-600 text-black block px-3 py-2 rounded-md text-base font-medium hover:text-heroText">About</h3></a></Link>
-                    <Link href="/#projects"><a><h3 className="cursor-pointer hover:text-heroText">Projects</h3></a></Link>
-                    <Link href="/#contact"><a><h3 className="cursor-pointer hover:text-heroText">Contact</h3></a></Link>
-                </div>) : null
-            }
-            </header>
+                <Transition
+                    show={isOpen}
+                    enter="transition ease-out duration-100 transform"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="transition ease-in duration-75 transform"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+
+                    <div className="md:hidden" id="mobile-menu">
+
+                        <Link
+                            href="/home"
+                            activeClass="home"
+                            to="home"
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            href="/about"
+                            activeClass="about"
+                            to="about"
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            About
+                        </Link>
+
+                        <Link
+                            href="/work"
+                            activeClass="work"
+                            to="work"
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Projects
+                        </Link>
+                        <Link
+                            href="/services"
+                            activeClass="services"
+                            to="services"
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Services
+                        </Link>
+
+                        <Link
+                            href="/contact"
+                            activeClass="work"
+                            to="work"
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                            className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Contact
+                        </Link>
+                    </div>
+
+
+                </Transition>
+            </nav>
         </div>
-    )
+    );
 }
