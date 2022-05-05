990bfd5dc0e4ff70d5b4294fe94a62c984c3b792
Bwise1 <oguntoyebenjamin2@gmail.com>
2022-05-05 12:22:24 +0300

    site title

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Contact from "../components/Forms/Contact";
@@ -8,6 +9,10 @@ import Projects from "../components/Project/Projects";
 export default function Home() {
   return (
     <div className="relative bg-white overflow-hidden">
+      <Head>
+        <title>Favsys</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Hero />
       {/*Projects Section*/}
