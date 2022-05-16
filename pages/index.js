4f345e137950ca88840d70fb8471b163c8f799da
Bwise1 <oguntoyebenjamin2@gmail.com>
2022-05-16 15:19:10 +0300

    changed favicon

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <Head>
         <title>Favsys</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Hero />
